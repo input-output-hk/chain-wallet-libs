7e7a4c8d4484b2e3776a5e25cc5ac00cc6356cd0
danielsanchezq <daniel.sanchez@iohk.io>
2020-04-17 13:35:50 +0200

    Change download script to support local builds

--- wallet-android/app/src/main/cpp/vendors/download.sh
+++ wallet-android/app/src/main/cpp/vendors/download.sh
@@ -5,6 +5,9 @@ VERSION="v0.2.0"
 LIST="aarch64-linux-android arm-linux-androideabi armv7-linux-androideabi i686-linux-android x86_64-linux-android"
 
 for target in $LIST; do
+  if [ -f "${target}/iohk_wallet/lib/libjormungandrwallet.*" ]; then
+    echo "Lib already present"
+  else
     ARCHIVE=chain-wallet-libs-${VERSION}-${target}.tar.gz
     URL="${BASE_URL}/${VERSION}/${ARCHIVE}"
     mkdir -p $target
@@ -12,4 +15,5 @@ for target in $LIST; do
     wget ${URL}
     tar xf ${ARCHIVE}
     popd
+  fi
 done
