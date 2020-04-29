27b33436a9346489bde7076698580429ed91f300
danielsanchezq <daniel.sanchez@iohk.io>
2020-04-29 16:09:31 +0200

    Get time_era from ledger Expose settings time_era

--- wallet/src/blockchain.rs
+++ wallet/src/blockchain.rs
@@ -5,11 +5,14 @@ use chain_impl_mockchain::{
     ledger::{Error, Ledger, LedgerParameters, LedgerStaticParameters},
     transaction::Input,
 };
+use chain_time::TimeEra;
+use std::time::{Duration, SystemTime};
 
 #[derive(Clone)]
 pub struct Settings {
     pub static_parameters: LedgerStaticParameters,
     pub parameters: LedgerParameters,
+    pub time_era: TimeEra,
 }
 
 impl Settings {
@@ -19,10 +22,12 @@ impl Settings {
 
         let static_parameters = ledger.get_static_parameters().clone();
         let parameters = ledger.get_ledger_parameters();
+        let time_era = ledger.era().clone();
 
         Ok(Self {
             static_parameters,
             parameters,
+            time_era,
         })
     }
 
@@ -37,11 +42,15 @@ impl Settings {
 
     // extract the block0 date-time
     // seconds in unix time (seconds elapsed since 1-Jan-1970)
-    pub fn start_date_time(&self) -> u64 {
-        self.static_parameters.block0_start_time.0
+    pub fn start_date_time(&self) -> SystemTime {
+        SystemTime::UNIX_EPOCH + Duration::from_secs(self.static_parameters.block0_start_time.0)
     }
 
     pub fn discrimination(&self) -> Discrimination {
         self.static_parameters.discrimination
+    }
+
+    pub fn time_era(&self) -> &TimeEra {
+        &self.time_era
     }
 }
