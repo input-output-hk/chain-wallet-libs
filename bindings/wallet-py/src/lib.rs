f1c97348ae1e0d5678c96910e69282e959ad241c
Daniel Sanchez <daniel.sanchez@iohk.io>
2020-07-12 12:43:34 +0200

    Added base python example bindings building working on python interpreter

---
+++ bindings/wallet-py/src/lib.rs
@@ -0,0 +1,19 @@
+use wallet_core::c::*;
+use pyo3::prelude::*;
+use pyo3::exceptions;
+use std::ptr::null_mut;
+
+#[pymodule]
+fn pyjormungandrwallet(_py: Python, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m, "wallet_recover")]
+    fn sum_as_string_py(_py: Python, mnemonics: &str, password: &[u8], password_length: usize) -> PyResult<()> {
+        let mut wallet_ptr : WalletPtr = null_mut();
+        let res = unsafe { wallet_recover(mnemonics, password.as_ptr(), password_length, &mut wallet_ptr) };
+        if res.is_err() {
+            return PyResult::Err(exceptions::NotImplementedError::py_err("No Implemented"));
+        }
+        wallet_delete_wallet(wallet_ptr);
+        PyResult::Ok(())
+    }
+    Ok(())
+}
