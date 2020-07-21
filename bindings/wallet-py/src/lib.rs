3267c53b498310250af8a972533bd16eff5d21bd
danielsanchezq <daniel.sanchez@iohk.io>
2020-07-21 13:19:15 +0200

    Implemented PyWallet Implemented Conversion

--- bindings/wallet-py/src/lib.rs
+++ bindings/wallet-py/src/lib.rs
@@ -1,6 +1,5 @@
 use pyo3::exceptions;
 use pyo3::prelude::*;
-use std::ptr::null_mut;
 use wallet_core;
 
 #[pyclass]
@@ -16,6 +15,11 @@ pub struct Settings {
 #[pyclass]
 pub struct Conversion {
     conversion: wallet_core::Conversion,
+}
+
+#[pyclass]
+pub struct Proposal {
+    proposal: wallet_core::Proposal,
 }
 
 #[pymethods]
@@ -50,11 +54,114 @@ impl PyWallet {
         })
     }
 
+    /// retrieve funds from daedalus or yoroi wallet in the given block0 (or
+    /// any other blocks).
+    ///
+    /// Execute this function then you can check who much funds you have
+    /// retrieved from the given block.
+    ///
+    /// this function may take sometimes so it is better to only call this
+    /// function if needed.
+    ///
+    /// also, this function should not be called twice with the same block.
     pub fn retrieve_funds(&mut self, block0: &[u8]) -> PyResult<Settings> {
         self.wallet
             .retrieve_funds(block0)
             .map_err(|e| exceptions::Exception::py_err(e.to_string()))
             .map(|settings| Settings { settings })
+    }
+
+    /// get the total value in the wallet
+    ///
+    /// make sure to call `retrieve_funds` prior to calling this function
+    /// otherwise you will always have `0`
+    pub fn total_value(&self) -> u64 {
+        self.wallet.total_value().0
+    }
+
+    /// update the wallet account state
+    ///
+    /// this is the value retrieved from any jormungandr endpoint that allows to query
+    /// for the account state. It gives the value associated to the account as well as
+    /// the counter.
+    ///
+    /// It is important to be sure to have an updated wallet state before doing any
+    /// transactions otherwise future transactions may fail to be accepted by any
+    /// nodes of the blockchain because of invalid signature state.
+    ///
+    pub fn set_state(&mut self, value: u64, counter: u32) {
+        self.wallet.set_state(wallet_core::Value(value), counter);
+    }
+
+    /// Cast a vote
+    ///
+    /// This function outputs a fragment containing a voting transaction.
+    ///
+    /// # Parameters
+    ///
+    /// * `settings` - ledger settings.
+    /// * `proposal` - proposal information including the range of values
+    ///   allowed in `choice`.
+    /// * `choice` - the option to vote for.
+    ///
+    /// # Errors
+    ///
+    /// The error is returned when `choice` does not fall withing the range of
+    /// available choices specified in `proposal`.
+    pub fn vote(
+        &mut self,
+        settings: &Settings,
+        proposal: &Proposal,
+        choice: u8,
+    ) -> PyResult<Vec<u8>> {
+        self.wallet
+            .vote(
+                settings.settings.clone(),
+                &proposal.proposal,
+                wallet_core::Choice::new(choice),
+            )
+            .map_err(|e| exceptions::Exception::py_err(e.to_string()))
+            .map(|res| res.as_ref().to_vec())
+    }
+}
+
+#[pymethods]
+impl Conversion {
+    /// retrieve the total number of ignored UTxOs in the conversion
+    /// transactions
+    ///
+    /// this is the number of utxos that are not included in the conversions
+    /// because it is more expensive to use them than to ignore them. This is
+    /// called dust.
+    pub fn num_ignored(&self) -> usize {
+        self.conversion.ignored().len()
+    }
+
+    /// retrieve the total value lost in dust utxos
+    ///
+    /// this is the total value of all the ignored UTxOs because
+    /// they are too expensive to use in any transactions.
+    ///
+    /// I.e. their individual fee to add as an input is higher
+    /// than the value they individually holds
+    pub fn total_value_ignored(&self) -> u64 {
+        self.conversion
+            .ignored()
+            .iter()
+            .map(|i| *i.value().as_ref())
+            .sum::<u64>()
+    }
+
+    /// the number of transactions built for the conversion
+    pub fn transactions_len(&self) -> usize {
+        self.conversion.transactions().len()
+    }
+
+    pub fn transactions_get(&self, index: usize) -> Option<Vec<u8>> {
+        self.conversion
+            .transactions()
+            .get(index)
+            .map(|t| t.to_owned())
     }
 }
 
