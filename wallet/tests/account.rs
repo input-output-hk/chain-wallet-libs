f3b37e6de80a86b31f4bd1f80bc6f1fa6580ba60
Enzo Cioppettini <ecioppettini@atixlabs.com>
2021-11-26 14:20:56 -0300

    add more exhaustive lane tests

--- wallet/tests/account.rs
+++ wallet/tests/account.rs
@@ -9,7 +9,6 @@ use chain_impl_mockchain::{
     value::Value,
     vote::{Choice, Payload},
 };
-use std::convert::TryInto;
 use wallet::{RecoveryBuilder, MAX_LANES};
 
 const BLOCK0: &[u8] = include_bytes!("../../test-vectors/block0");
@@ -57,50 +56,45 @@ fn cast_vote() {
     let settings = state.settings().expect("valid initial settings");
 
     for fragment in state.initial_contents() {
-        account.check_fragment(&fragment.hash(), fragment);
+        account.check_fragment(&fragment.hash(), fragment).unwrap();
         account.confirm(&fragment.hash());
     }
 
-    let vote_plan_id: [u8; 32] = hex::decode(
-        "784d95bf9090969df0398f94c48baffbba8ea9f6e7a1e7d808a156330fdf33e1",
-    )
-    .unwrap()[..]
-        .try_into()
-        .unwrap();
+    let vote_plan_id = &state.active_vote_plans()[0];
 
-    let index = 0;
     let choice = Choice::new(1);
-
-    let payload = Payload::Public { choice };
-
-    let cast = VoteCast::new(vote_plan_id.into(), index, payload);
 
     let current_time =
         std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(settings.block0_date.0);
 
-    let mut builder = wallet::TransactionBuilder::new(
-        &settings,
-        cast,
-        wallet::time::max_expiration_date(&settings, current_time).unwrap(),
-    );
+    for i in 0..16 {
+        let payload = Payload::Public { choice };
+        let cast = VoteCast::new(vote_plan_id.clone().into(), i, payload);
 
-    let value = builder.estimate_fee_with(1, 0);
+        let mut builder = wallet::TransactionBuilder::new(
+            &settings,
+            cast.clone(),
+            wallet::time::max_expiration_date(&settings, current_time).unwrap(),
+        );
 
-    let account_tx_builder = account.new_transaction(value, 0).unwrap();
-    let input = account_tx_builder.input();
-    let witness_builder = account_tx_builder.witness_builder();
+        let value = builder.estimate_fee_with(1, 0);
 
-    builder.add_input(input, witness_builder);
+        let account_tx_builder = account.new_transaction(value, i % 8).unwrap();
+        let input = account_tx_builder.input();
+        let witness_builder = account_tx_builder.witness_builder();
 
-    let tx = builder.finalize_tx(()).unwrap();
+        builder.add_input(input, witness_builder);
 
-    let fragment = Fragment::VoteCast(tx);
-    let raw = fragment.to_raw();
-    let id = raw.id();
+        let tx = builder.finalize_tx(()).unwrap();
 
-    account_tx_builder.add_fragment_id(id);
+        let fragment = Fragment::VoteCast(tx);
+        let raw = fragment.to_raw();
+        let id = raw.id();
 
-    state
-        .apply_fragments(&[raw])
-        .expect("couldn't apply votecast fragment");
+        account_tx_builder.add_fragment_id(id);
+
+        state
+            .apply_fragments(&[raw])
+            .expect("couldn't apply votecast fragment");
+    }
 }
